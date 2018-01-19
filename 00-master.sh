cf2da00e9017e5281eb70d19fa26e95a76a042fd
Sharon Rolel <sharon.rolel@activeallocator.com>
2018-01-19 07:29:18 -0500

    bash path

--- 00-master.sh
+++ 00-master.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 set -o nounset -o errexit
 
 kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=${MASTER_PRIVATE_IP} --apiserver-cert-extra-sans=${MASTER_PUBLIC_IP}
